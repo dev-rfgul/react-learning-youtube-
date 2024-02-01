79f39bec3ddc89c3babd9e1aee419a7211c5d5fd
dev-rfgul <rfgul587@gmail.com>
2024-02-01 20:48:52 +0500

    changed some animation settings

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -5,25 +5,25 @@ function Products() {
   return (
     <>
       <div className="flex relative align-middle justify-center items-center mb-5 ">
-        <motion.div drag dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }} className="text-white w-44 h-32 bg-red-800 rounded-xl p-5 m-5   items-center justify-center">
+        <motion.div drag dragConstraints={{ left: -100, right: 100, top: -100, bottom: 100 }} className="text-white w-44 h-32 bg-red-800 rounded-xl p-5 m-5   items-center justify-center">
           hello i am product component made by fahad
         </motion.div>
 
-        <motion.div  drag dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }} className="text-white w-44 h-32 bg-blue-800 rounded-xl p-5 m-5  items-center justify-center">
+        <motion.div  drag dragConstraints={{ left: -100, right: 100, top: -100, bottom: 100 }} className="text-white w-44 h-32 bg-blue-800 rounded-xl p-5 m-5  items-center justify-center">
           hello i am product component made by fahad
         </motion.div>
 
-        <motion.div drag dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}  className="text-white w-44 h-32 bg-yellow-800 rounded-xl p-5 m-5  items-center justify-center">
+        <motion.div drag dragConstraints={{ left: -100, right: 100, top: -100, bottom: 100 }}  className="text-white w-44 h-32 bg-yellow-800 rounded-xl p-5 m-5  items-center justify-center">
           hello i am product component made by fahad
         </motion.div>
       </div>
 
       <div className="flex relative align-middle justify-center items-center mb-5">
-        <motion.div  drag dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}  className="text-white w-44 h-32 bg-pink-800 rounded-xl p-5 m-5  items-center justify-center">
+        <motion.div  drag dragConstraints={{ left: -100, right: 100, top: -100, bottom: 100 }}  className="text-white w-44 h-32 bg-pink-800 rounded-xl p-5 m-5  items-center justify-center">
           hello i am product component made by fahad
         </motion.div>
 
-        <motion.div drag dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}   className="text-white w-44 h-32 bg-purple-800 rounded-xl p-5 m-5  items-center justify-center">
+        <motion.div drag dragConstraints={{ left: -100, right: 100, top: -100, bottom: 100 }}   className="text-white w-44 h-32 bg-purple-800 rounded-xl p-5 m-5  items-center justify-center">
           hello i am product component made by fahad
         </motion.div>
       </div>
